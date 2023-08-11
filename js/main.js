576bff8134b600cba6286bbed8f31ea85537a31b
aksym Nechaiev <maximus.ukr@gmail.com>
2023-08-11 13:27:10 +0300

    Added "dynamic" include of header and footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
+(async () => {
+	const headerResponse = await fetch('/header.html');
+    const headerText = await headerResponse.text();
+	const headerPlaceholser = document.querySelector('.headerPlaceholder')
+	if (headerPlaceholser) headerPlaceholser.innerHTML = headerText;
+	addHeaderHandlers()
+	
+    const footerResponse = await fetch('/footer.html');
+    const footerText = await footerResponse.text();
+	const footerPlaceholder = document.querySelector('.footerPlaceholder')
+	if (footerPlaceholder) footerPlaceholder.innerHTML = footerText;
+
+})();
+
+function addHeaderHandlers() {
+	let menuBtn = document.querySelector('.menu-btn');
+	let menu = document.querySelector('.menu');
+	
+	menuBtn.addEventListener('click', function(){
+		menuBtn.classList.toggle('active');
+		menu.classList.toggle('active');
+	});
+}
