0244e0eba2e340fa5f7d5a24db96da5b85178634
aksym Nechaiev <maximus.ukr@gmail.com>
2023-08-11 13:32:40 +0300

    Removed traces of placeholders in resulting DOM

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@
 	const headerResponse = await fetch('/header.html');
     const headerText = await headerResponse.text();
 	const headerPlaceholser = document.querySelector('.headerPlaceholder')
-	if (headerPlaceholser) headerPlaceholser.innerHTML = headerText;
+	if (headerPlaceholser) headerPlaceholser.outerHTML = headerText;
 	addHeaderHandlers()
 	
     const footerResponse = await fetch('/footer.html');
     const footerText = await footerResponse.text();
 	const footerPlaceholder = document.querySelector('.footerPlaceholder')
-	if (footerPlaceholder) footerPlaceholder.innerHTML = footerText;
+	if (footerPlaceholder) footerPlaceholder.outerHTML = footerText;
 
 })();
 
