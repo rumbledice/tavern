be17ff87f7351ca2106d72a71a92f471c872e988
rumbledice <114624189+rumbledice@users.noreply.github.com>
2023-09-13 01:15:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
+(async () => {
+	const headerResponse = await fetch('/header.html');
+    const headerText = await headerResponse.text();
+	const headerPlaceholser = document.querySelector('.headerPlaceholder')
+	if (headerPlaceholser) headerPlaceholser.innerHTML = headerText;
+     else {
+        const alternativeHeaderResponse = await fetch('/header1.html');
+        const alternativeHeaderText = await alternativeHeaderResponse.text();
+        const alternativeHeaderPlaceholder = document.querySelector('.headerPlaceholder1');
+        if (alternativeHeaderPlaceholder) {
+            alternativeHeaderPlaceholder.innerHTML = alternativeHeaderText;
+        }
+    }
+	addHeaderHandlers()
+
+
+    const footerResponse = await fetch('/footer.html');
+    const footerText = await footerResponse.text();
+	const footerPlaceholder = document.querySelector('.footerPlaceholder')
+	if (footerPlaceholder) footerPlaceholder.innerHTML = footerText;
+
+})();
+
+function addHeaderHandlers() {
+	let menuBtn = document.querySelector('.menu-btn');
+	let menu = document.querySelector('.menu');
+	
+	menuBtn.addEventListener('click', function(){
+		menuBtn.classList.toggle('active');
+		menu.classList.toggle('active');
+	});
+}
